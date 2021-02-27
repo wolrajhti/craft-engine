6abf63c713fe04ef4d323b35d78ca9e5ed9da9d4
wolrajhti <pierre.bouteloup@hotmail.fr>
2021-02-27 12:04:41 +0100

    feat: tests for .dot and .cross

--- __tests__/domain/vector2.ts
+++ __tests__/domain/vector2.ts
@@ -27,5 +27,33 @@ describe('Vector2', () => {
     test('cas 1: basic diagonal', () => {
       expect(a.normalize().equals(new Vector2(1, 1).normalize())).toBeTruthy();
     });
-  })
+  });
+  describe('dot', () => {
+    test('cas 1: basic ortho', () => {
+      expect(new Vector2(1, 0).dot(new Vector2(0, 1))).toBe(0);
+    });
+    test('cas 2: basic ortho negative', () => {
+      expect(new Vector2(0, 1).dot(new Vector2(1, 0))).toBe(0);
+    });
+    test('cas 3: flat', () => {
+      expect(new Vector2(1, 0).dot(new Vector2(-1, 0))).toBe(-1);
+    });
+    test('cas 4: flat', () => {
+      expect(new Vector2(1, 0).dot(new Vector2(1, 0))).toBe(1);
+    });
+  });
+  describe('cross', () => {
+    test('cas 1: basic ortho', () => {
+      expect(new Vector2(1, 0).cross(new Vector2(0, 1))).toBe(1);
+    });
+    test('cas 2: basic ortho negative', () => {
+      expect(new Vector2(0, 1).cross(new Vector2(1, 0))).toBe(-1);
+    });
+    test('cas 3: flat', () => {
+      expect(new Vector2(1, 0).cross(new Vector2(-1, 0))).toBe(0);
+    });
+    test('cas 4: flat', () => {
+      expect(new Vector2(1, 0).cross(new Vector2(3, 0))).toBe(0);
+    });
+  });
 });
